837a0674b40300ee3345a041a9940a16a0b9d771
Shadman Shaharia <alhamdullilah@Shadmans-MacBook-Air.local>
2021-11-18 10:03:43 -0500

    11/18 work

---
+++ WordPairList/WordPair.java
@@ -0,0 +1,17 @@
+public class WordPair{
+
+private String a, b;
+
+public WordPair(String first, String second){
+a = first;
+b = second;
+}
+
+public String getFirst(){
+return a;
+}
+
+public String getSecond(){
+return b;
+}
+}
