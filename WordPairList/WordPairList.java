ad74f71bc7d3798e01456fcec98f2a85b3d7cb15
Shadman Shaharia <alhamdullilah@Shadmans-MacBook-Air.local>
2021-11-18 10:13:03 -0500

    Unfinished

---
+++ WordPairList/WordPairList.java
@@ -0,0 +1,9 @@
+import.java.util
+public class WordPairList{
+
+    private ArrayList<WordPair> allPairs;
+
+     public WordPairList(String[] words){
+          allPairs = new ArrayList<WordPair>();
+     }
+}
