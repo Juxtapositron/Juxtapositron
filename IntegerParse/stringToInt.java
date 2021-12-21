f10ae0705372bcb8c6dfeb5fc910c556677f9964
Shadman Shaharia <sshaharia30@stuy.edu>
2021-12-21 10:15:26 -0500

    ok

---
+++ IntegerParse/stringToInt.java
@@ -0,0 +1,15 @@
+public class stringToInt{
+
+  public int valueOfDigit(char c){
+    if (c - 48 <= -1 && c - 48 < 9){
+      throw new IllegalArgumentException;
+    } else return c - 48;
+  }
+
+  public int stringToInt(String s){
+    int x = 0;
+    for (int i = 0; i<s.length(); i++){
+      
+    }
+  }
+}
