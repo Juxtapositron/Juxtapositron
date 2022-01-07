f631872b02e81236d262b64df289df457d049427
Shadman Shaharia <sshaharia30@stuy.edu>
2022-01-07 10:12:51 -0500

    we start here

---
+++ GameStuff/Wizard.java
@@ -0,0 +1,63 @@
+public class Wizard extends Adventurer {
+    private int  rage;
+    private String warcry;
+
+    public Wizard(){
+	     this("Perseus");
+    }
+
+    public Wizard(String name){
+	     this(name,"With the power of sorcery!", 18);
+    }
+
+    public Wizard(String name, String warcry, int rage){
+      super(name,30+(int)(Math.random()*10));
+      setWarcry(warcry);
+      setRage(rage);
+    }
+
+    public int compareTo(Wizard other){
+	     return getHP() - other.getHP();
+    }
+
+    //wizard methods
+
+    public void attack(Damageable other){
+    	  int damage = (int)(Math.random()*10)+1;
+  	    other.applyDamage(damage);
+  	    setRage(getRage() + 1);
+  	    System.out.println(this +
+            " attacked " + other + " for " +
+            damage + " damage!");
+    }
+
+    public void specialAttack(Damageable other){
+	     if(getRage() >= 10){
+  	        int damage = (int)(Math.random()*20)+1;
+            other.applyDamage(damage);
+            System.out.println(this + " unleashes his fury upon "
+             + other + " for " + damage + " damage! "+warcry);
+            setRage(getRage() - 10);
+	    }else{
+			    System.out.println("Not enough rage! ");
+          attack(other);
+	    }
+    }
+
+    //get methods
+
+    public int getRage(){
+	     return rage;
+    }
+
+    //set methods
+    public void setRage(int r){
+	     this.rage = r;
+    }
+
+    public void setWarcry(String warcry){
+	     this.warcry = warcry;
+    }
+
+
+}
